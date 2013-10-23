e39739f2d4c46413fc60f3f85c20b79a83486fb8
Guilherme Fontenele <guilherme@fontenele.net>
2013-10-23 07:23:43 -0700

    Work without virtual host

--- module/Services/config/module.config.php
+++ module/Services/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -7,28 +8,9 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 return array(
-    'router' => array(
-        'routes' => array(
-            'services' => array(
-                'type' => 'segment',
-                'options' => array(
-                    'route' => '/services[/:controller][/:action][/:id]',
-                    'constraints' => array(
-                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id' => '[0-9]+'
-                    ),
-                    'defaults' => array(
-                        'controller' => 'service',
-                        'action' => 'rubix'
-                    )
-                )
-            ),
-        )
-    ),
-    /*'view_manager' => array(
+    'view_manager' => array(
         'template_path_stack' => array(
             'usuarios' => __DIR__ . '/../view'
         )
-    ),*/
+    ),
 );
