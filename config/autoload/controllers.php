3f2781bef0788c4c48eed8debe8ceeccfd9e2d88
Guilherme Fontenele <guilherme@fontenele.net>
2013-10-27 22:27:16 -0700

    controller

--- config/autoload/controllers.php
+++ config/autoload/controllers.php
@@ -3,6 +3,7 @@
 return array(
     'invokables' => array(
         'home' => 'Main\Controller\IndexController',
+        'perfil' => 'Main\Controller\PerfilController',
         'service' => 'Services\Controller\ServiceController',
         'phpunit' => 'Test\Controller\PhpUnitController'
     )
